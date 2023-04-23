91c7e59baad68d648a65c4ad38cfe09a625dd704
Shashank Yadav <yadavshashank70@gmail.com>
2023-04-24 01:05:01 +0530

    scenario 2 adding user class and borrowing function

--- libraryManagement.js
+++ libraryManagement.js
@@ -17,6 +17,37 @@ class Library {
             });
         }
     }
+
+    removeBook(book) {
+        const index = this.books.findIndex(b => b === book);
+        if (index !== -1) {
+            this.books.splice(index, 1);
+        } else {
+            console.log("Book not found.");
+        }
+    }
+}
+
+class User {
+    constructor() {
+        this.borrowedBooks = [];
+        this.borrowingLimit = 2;
+    }
+
+    borrowBook(book, library) {
+        if (this.borrowedBooks.length < this.borrowingLimit) {
+            const index = library.books.findIndex(b => b === book);
+            if (index !== -1) {
+                library.removeBook(book);
+                this.borrowedBooks.push(book);
+                console.log(`You have borrowed ${book}`);
+            } else {
+                console.log("Book not found in library.");
+            }
+        } else {
+            console.log("You have reached your borrowing limit.");
+        }
+    }
 }
 
 //Scenario 1: 
@@ -26,7 +57,7 @@ class Library {
 const emptyLibrary = new Library();
 emptyLibrary.viewBooks();
 
-//Given, there are books in the library
+// Given, there are books in the library
 // When, I view the books in the library
 // Then, I see the list of books in the library
 const library1 = new Library();
@@ -34,3 +65,25 @@ library1.addBook('book1');
 library1.addBook('book2');
 library1.addBook('book3');
 library1.viewBooks();
+
+// Scenario 2:  User can borrow a book from the library
+// Given, there are books in the library
+// When, I choose a book to add to my borrowed list
+// Then, the book is added to my borrowed list
+// And, the book is removed from the library
+
+const scenario_2_Library = new Library();
+scenario_2_Library.addBook('book1');
+scenario_2_Library.addBook('book2');
+scenario_2_Library.addBook('book3');
+
+const user_one = new User();
+user_one.borrowBook('book1', scenario_2_Library);
+user_one.borrowBook('book2', scenario_2_Library);
+user_one.borrowBook('book3', scenario_2_Library);
+
+// View borrowed books
+console.log("Borrowed books List:");
+user_one.borrowedBooks.forEach((book, index) => {
+    console.log(`${index + 1} - ${book}`);
+});
