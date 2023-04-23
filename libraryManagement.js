87f0be74ce2087e22e9e5f70fad92de0a92dd3dc
Shashank Yadav <yadavshashank70@gmail.com>
2023-04-24 01:11:50 +0530

    added documention and supporting comments

--- libraryManagement.js
+++ libraryManagement.js
@@ -1,12 +1,15 @@
+// Library class structure
 class Library {
     constructor() {
         this.books = [];
     }
 
+    // function for adding books in library
     addBook(book) {
         this.books.push(book);
     }
 
+    // function for viewing books 
     viewBooks() {
         if (this.books.length === 0) {
             console.log("Library is empty! contact the manager.");
@@ -18,6 +21,7 @@ class Library {
         }
     }
 
+    // function for removing books from library once borrowed by user
     removeBook(book) {
         const index = this.books.findIndex(b => b === book);
         if (index !== -1) {
@@ -31,9 +35,10 @@ class Library {
 class User {
     constructor() {
         this.borrowedBooks = [];
-        this.borrowingLimit = 2;
+        this.borrowingLimit = 2; //Max borrowing limit per user set to 2
     }
 
+    // function for borrowing books from library 
     borrowBook(book, library) {
         if (this.borrowedBooks.length < this.borrowingLimit) {
             const index = library.books.findIndex(b => b === book);
@@ -54,12 +59,16 @@ class User {
 // Given, there are no books in the library
 // When, I view the books in the library
 // Then, I see an empty library
+
+// creating empty library for scenario 1
 const emptyLibrary = new Library();
 emptyLibrary.viewBooks();
 
 // Given, there are books in the library
 // When, I view the books in the library
 // Then, I see the list of books in the library
+
+// creating filled library for scenario 1
 const library1 = new Library();
 library1.addBook('book1');
 library1.addBook('book2');
@@ -72,12 +81,16 @@ library1.viewBooks();
 // Then, the book is added to my borrowed list
 // And, the book is removed from the library
 
+//creating new library for scenario 2
 const scenario_2_Library = new Library();
 scenario_2_Library.addBook('book1');
 scenario_2_Library.addBook('book2');
 scenario_2_Library.addBook('book3');
 
+// creating user using user class  
 const user_one = new User();
+
+// invoking borrow function on user_one
 user_one.borrowBook('book1', scenario_2_Library);
 user_one.borrowBook('book2', scenario_2_Library);
 user_one.borrowBook('book3', scenario_2_Library);
