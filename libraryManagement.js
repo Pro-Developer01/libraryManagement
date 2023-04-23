306bb7c4c1b47690e937075c3b26b50f79bb3e55
Shashank Yadav <yadavshashank70@gmail.com>
2023-04-24 00:10:10 +0530

    addBook function added and viewed unempty library

--- libraryManagement.js
+++ libraryManagement.js
@@ -3,6 +3,10 @@ class Library {
         this.books = [];
     }
 
+    addBook(book) {
+        this.books.push(book);
+    }
+
     viewBooks() {
         if (this.books.length === 0) {
             console.log("Library is empty! contact the manager.");
@@ -21,3 +25,12 @@ class Library {
 // Then, I see an empty library
 const emptyLibrary = new Library();
 emptyLibrary.viewBooks();
+
+//Given, there are books in the library
+// When, I view the books in the library
+// Then, I see the list of books in the library
+const library1 = new Library();
+library1.addBook('book1');
+library1.addBook('book2');
+library1.addBook('book3');
+library1.viewBooks();
